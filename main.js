8eb3950c2c4a8f7174a269e8c82c2ef3a1b14f3f
Juraci Vieira <juraci.vieira@gmail.com>
2016-01-25 22:26:56 -0200

    More madness

---
+++ main.js
@@ -0,0 +1,82 @@
+function simulateClick(element) {
+    var options = { bubbles: true, cancelable: true, view: window   };
+    var mouseUpEvent = new MouseEvent('mousedown',  options);
+    var mouseDownEvent = new MouseEvent('mouseup', options);
+
+    element.dispatchEvent(mouseDownEvent);
+    element.dispatchEvent(mouseUpEvent);
+    element.dispatchEvent(mouseDownEvent);
+}
+
+function sanitizeTime(str) {
+    return str.replace(/ \([\d+]*\,*[\d+]* [a-z]+\)/, '');
+}
+
+function selectTime(time, element) {
+
+    simulateClick(element);
+    var hours = document.querySelectorAll('div.goog-control');
+    var sanitizedTime = '';
+
+    for(var i = 0; i < hours.length; i++) {
+        sanitizedTime = sanitizeTime(hours[i].textContent);
+        if (sanitizedTime.includes(time)) {
+            simulateClick(hours[i]);
+            return 'Hour selected: ' + sanitizedTime;
+        }
+    }
+
+    return 'Hour not found: ' + time;
+}
+
+function clickOnRooms() {
+    var rooms = document.querySelector('#ui-ltsr-tab-1');
+    rooms.click();
+}
+
+function clickOnCountry(country) {
+    var elements = document.querySelectorAll('div.ch');
+    for(var i =  0; i < elements.length; i++) {
+        if (elements[i].textContent.includes(country)) {
+            elements[i].click();
+            return 'Country found: ' + country;
+        }
+
+    }
+    return 'Country ' + country + ' not found';
+}
+
+function findRoomInOffice(query) {
+    var tries = 0;
+    var maxTries = 20;
+
+    var id = setInterval(function(){
+        console.log('looking for offices');
+        var offices = document.querySelectorAll('div.ci[style*="background-image"][style*="res_a.gif"]');
+        if (offices.length > 0) {
+            console.log('Offices found');
+            for(var i = 0; i < offices.length; i++) {
+                console.log('Checking office: ' + offices[i].textContent);
+                if(offices[i].textContent.includes(query)) {
+                    console.log('Room found ' + query);
+                    offices[i].querySelector('.conf-action').click();
+                    clearInterval(id);
+                }
+            }
+        }
+
+        tries++;
+        if (tries >= maxTries) {
+            console.log('Could not find the office in %s tries, aborting.', tries);
+            clearInterval(id);
+        }
+    }, 800);
+}
+
+var startTime = document.querySelector('input[id*="st"][class*="dr-time"]');
+var endTime = document.querySelector('input[id*="et"][class*="dr-time"]');
+console.log(selectTime('16:30', startTime));
+console.log(selectTime('17:30', endTime));
+clickOnRooms();
+console.log(clickOnCountry('Brazil'));
+findRoomInOffice('POA');
