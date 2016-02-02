7af418a2bf536aceaa608a724af63562b0111099
Juraci Vieira <juraci.vieira@gmail.com>
2016-02-01 22:48:16 -0200

    adding #twelveHoursFormat method

--- spec/timeMachineSpec.js
+++ spec/timeMachineSpec.js
@@ -1,18 +1,32 @@
 describe('TimeMachine', function() {
     var TimeMachine = require('../lib/TimeMachine.js');
 
-    it('detects the twenty four hours format', function() {
-        var time = new TimeMachine('10:00');
-        expect(time.twentyFourHoursFormat()).toBe(true);
+    describe('#twentyFourHoursFormat', function() {
+        it('returns true to 17:00', function() {
+            var time = new TimeMachine('17:00');
+            expect(time.twentyFourHoursFormat()).toBe(true);
+        });
+
+        it('returns false to 10:00am', function() {
+            var time = new TimeMachine('10:00am');
+            expect(time.twentyFourHoursFormat()).toBe(false);
+        });
+
+        it('returns false to 6:00pm', function() {
+            var time = new TimeMachine('6:00pm');
+            expect(time.twentyFourHoursFormat()).toBe(false);
+        });
     });
 
-    it('10:00am should not be detected as twenty four hours format', function() {
-        var time = new TimeMachine('10:00am');
-        expect(time.twentyFourHoursFormat()).toBe(false);
-    });
+    describe('#twelveHoursFormat', function() {
+        it('returns true to 6:00pm', function() {
+            var time = new TimeMachine('6:00pm');
+            expect(time.twelveHoursFormat()).toBe(true);
+        });
 
-    it('6:00pm should not be detected as twenty four hours format', function() {
-        var time = new TimeMachine('6:00pm');
-        expect(time.twentyFourHoursFormat()).toBe(false);
+        it('returns false to 11:00', function() {
+            var time = new TimeMachine('11:00');
+            expect(time.twelveHoursFormat()).toBe(false);
+        });
     });
 });
