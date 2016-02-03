c2692f960357aa6f50d22b824bf2abc69f6069bf
Juraci Vieira <juraci.vieira@gmail.com>
2016-02-03 13:24:57 -0200

    Adding addDuration mehtod to TimeMachine

--- spec/timeMachineSpec.js
+++ spec/timeMachineSpec.js
@@ -63,4 +63,83 @@ describe('TimeMachine', function() {
             expect(time.getMinutes()).toEqual(30);
         });
     });
+
+    describe('#getExtension', function() {
+        it('returns pm when the time is 12:00pm', function() {
+            var time = new TimeMachine('12:00pm');
+            expect(time.getExtension()).toEqual('pm');
+        });
+
+        it('returns am when the time is 11:30am', function() {
+            var time = new TimeMachine('11:30am');
+            expect(time.getExtension()).toEqual('am');
+        });
+
+        it('returns false when it is a twenty four hours time format', function() {
+            var time = new TimeMachine('11:30');
+            expect(time.getExtension()).toBe(false);
+        });
+    });
+
+    describe('#incrementByOneHour', function() {
+        describe('when the format is twenty four hours', function() {
+            it('sets the final time to 13:00 when the initial time is 12:00', function() {
+                var time = new TimeMachine('12:00');
+                time.incrementByOneHour();
+                expect(time.time).toEqual('13:00');
+            });
+        });
+
+        describe('when the format is twelve hours', function() {
+            it('sets the final time to 1:00pm when the initial time is 12:pm', function() {
+                var time = new TimeMachine('12:00pm');
+                time.incrementByOneHour();
+                expect(time.time).toEqual('1:00pm');
+            });
+
+            it('sets the final time to 12:30pm when the initial time is 11:30am', function() {
+                var time = new TimeMachine('11:30am');
+                time.incrementByOneHour();
+                expect(time.time).toEqual('12:30pm');
+            });
+
+            it('sets the final time to 12:00am when the initial time is 11:00pm', function() {
+                var time = new TimeMachine('11:00pm');
+                time.incrementByOneHour();
+                expect(time.time).toEqual('12:00am');
+            });
+
+            it('sets the final time to 3:00pm when the initial time is 2:00pm', function() {
+                var time = new TimeMachine('2:00pm');
+                time.incrementByOneHour();
+                expect(time.time).toEqual('3:00pm');
+            });
+        });
+    });
+
+    describe('#addDuration', function() {
+        it('sets the final time to 18:30 when the time is 17:30 and the duration is 1h', function() {
+            var time = new TimeMachine('17:30');
+            time.addDuration('1');
+            expect(time.time).toEqual('18:30');
+        });
+
+        it('sets the final time to 1:30pm when the time is 12:30pm and the duration is 1h', function() {
+            var time = new TimeMachine('12:30pm');
+            time.addDuration('1');
+            expect(time.time).toEqual('1:30pm');
+        });
+
+        it('sets the final time to 12:30pm when the time is 11:30am and the duration is 1h', function() {
+            var time = new TimeMachine('11:30am');
+            time.addDuration('1');
+            expect(time.time).toEqual('12:30pm');
+        });
+
+        it('sets the final time to 16:00 when the time is 14:00 and the duration is 2h', function() {
+            var time = new TimeMachine('14:00');
+            time.addDuration('2');
+            expect(time.time).toEqual('16:00');
+        });
+    });
 });
